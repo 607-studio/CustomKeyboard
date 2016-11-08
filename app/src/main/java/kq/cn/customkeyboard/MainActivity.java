7d63ca6100073e9fd7f2479bdbfc7bc81a9d208c
kangqiao610 <kangqiao610@gmail.com>
2016-11-08 13:33:31 +0800

    支持自定义每一个Key的样式

--- app/src/main/java/kq/cn/customkeyboard/MainActivity.java
+++ app/src/main/java/kq/cn/customkeyboard/MainActivity.java
@@ -1,5 +1,7 @@
 package kq.cn.customkeyboard;
 
+import android.graphics.drawable.Drawable;
+import android.inputmethodservice.Keyboard;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -16,8 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.et_input_price)
     EditText etInputPrice;
-    @BindView(R.id.et_input_num)
-    EditText etInputNum;
+    @BindView(R.id.et_input_num_buy)
+    EditText etInputNumBuy;
+    @BindView(R.id.et_input_num_sell)
+    EditText etInputNumSell;
     @BindView(R.id.layout_opt_container)
     LinearLayout optContainer;
     @BindView(R.id.view_under)
@@ -44,54 +48,83 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         customKeyboardManager = new CustomKeyboardManager(this);
 
-        CustomKeyboardManager.BaseKeyboard priceKeyboard = new CustomKeyboardManager.BaseKeyboard(this, R.xml.stock_price_num_keyboard) {
+        CustomBaseKeyboard priceKeyboard = new CustomBaseKeyboard(this, R.xml.stock_price_num_keyboard) {
             @Override
             public boolean handleSpecialKey(EditText etCurrent, int primaryCode) {
-                if (primaryCode == getKeyCode( R.integer.keycode_cur_price)) {
-                    etCurrent.setText("9.99");
+                if (primaryCode == getKeyCode(R.integer.keycode_cur_price)) {
+                    etCurrent.setText("9.999$");
                     return true;
                 }
                 return false;
             }
         };
 
-        customKeyboardManager.attachTo((EditText) $(R.id.et_input_price1), priceKeyboard);
-        customKeyboardManager.attachTo((EditText) $(R.id.et_input_price2), priceKeyboard);
-        //customKeyboardManager.attachTo((EditText) $(R.id.et_input_price3), priceKeyboard);
-        //customKeyboardManager.attachTo((EditText) $(R.id.et_input_price4), priceKeyboard);
-        //customKeyboardManager.attachTo((EditText) $(R.id.et_input_price5), priceKeyboard);
-        customKeyboardManager.attachTo((EditText) $(R.id.et_input_price6), priceKeyboard);
-
         customKeyboardManager.attachTo(etInputPrice, priceKeyboard);
 
-        customKeyboardManager.attachTo(etInputNum, new CustomKeyboardManager.BaseKeyboard(this, R.xml.stock_trade_num_keyboard) {
+        CustomBaseKeyboard numKeyboard = new CustomBaseKeyboard(this, R.xml.stock_trade_num_keyboard) {
             @Override
             public boolean handleSpecialKey(EditText etCurrent, int primaryCode) {
                 Editable editable = etCurrent.getText();
                 int start = etCurrent.getSelectionEnd();
-                if (primaryCode == getKeyCode( R.integer.keycode_stocknum_000)) {
+                if (primaryCode == getKeyCode(R.integer.keycode_stocknum_000)) {
                     editable.insert(start, "000");
                     return true;
-                } else if (primaryCode == getKeyCode(R.integer.keycode_stocknum_half)){
-                    etCurrent.setText(1000 /2+"");
+                } else if (primaryCode == getKeyCode(R.integer.keycode_stocknum_half)) {
+                    etCurrent.setText(1000 / 2 + "");
                     return true;
-                } else if (primaryCode == getKeyCode(R.integer.keycode_stocknum_all)){
+                } else if (primaryCode == getKeyCode(R.integer.keycode_stocknum_all)) {
                     setStockNumAll(etCurrent);
+                    return true;
+                } else if (primaryCode == getKeyCode(R.integer.keycode_stock_sell)) {
+                    etCurrent.setText("999999");
+                    hideKeyboard();
                     return true;
                 }
                 return false;
             }
+        };
+
+        numKeyboard.setCustomKeyStyle(new CustomBaseKeyboard.SimpleCustomKeyStyle() {
+            @Override
+            public Drawable getKeyBackground(Keyboard.Key key, EditText etCur) {
+                if (getKeyCode(etCur.getContext(), R.integer.keycode_stock_sell) == key.codes[0]) {
+                    if (R.id.et_input_num_sell == etCur.getId()) {
+                        return getDrawable(etCur.getContext(), R.drawable.bg_custom_key_blue);
+                    } else if (R.id.et_input_num_buy == etCur.getId()) {
+                        return getDrawable(etCur.getContext(), R.drawable.bg_custom_key_red);
+                    }
+                }
+                return super.getKeyBackground(key, etCur);
+            }
+
+            @Override
+            public CharSequence getKeyLabel(Keyboard.Key key, EditText etCur) {
+                if (getKeyCode(etCur.getContext(), R.integer.keycode_stock_sell) == key.codes[0]) {
+                    if (R.id.et_input_num_sell == etCur.getId()) {
+                        return "卖出";
+                    } else if (R.id.et_input_num_buy == etCur.getId()) {
+                        return "买入";
+                    }
+                }
+                return super.getKeyLabel(key, etCur);
+            }
+
+            @Override
+            public Integer getKeyTextColor(Keyboard.Key key, EditText etCur) {
+                if (getKeyCode(etCur.getContext(), R.integer.keycode_stock_sell) == key.codes[0]) {
+                    return getResources().getColor(R.color.default_white);
+                }
+                return super.getKeyTextColor(key, etCur);
+            }
         });
+        customKeyboardManager.attachTo(etInputNumBuy, numKeyboard);
+        customKeyboardManager.attachTo(etInputNumSell, numKeyboard);
+
         customKeyboardManager.setShowUnderView(underView);
     }
 
     @OnClick(R.id.tv_stock_all)
-    public void setStockNumAll(View view){
-        etInputNum.setText("10000");
-    }
-
-    @Deprecated
-    protected <T extends View> T $(int id) {
-        return (T) super.findViewById(id);
+    public void setStockNumAll(EditText view) {
+        view.setText("10000");
     }
 }
